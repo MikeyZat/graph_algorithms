48c71f810229e57da278a11ff7182cbc69cde6e9
MikeyZat <mikeyzat@gmail.com>
2019-12-02 11:11:54 +0100

    Finish lexBFS and start checking ON

---
+++ lab4/main.py
@@ -0,0 +1,72 @@
+from lab2.dimacs import *
+
+
+class Node:
+    def __init__(self, idx):
+        self.idx = idx
+        self.out = set()  # zbiór sąsiadów
+
+    def connect_to(self, v):
+        self.out.add(v)
+
+
+
+def checkLexBFS(G, vs):
+    n = len(G)
+    pi = [None] * n
+    for i, v in enumerate(vs):
+        pi[v] = i
+
+    for i in range(n - 1):
+        for j in range(i + 1, n - 1):
+            Ni = G[vs[i]].out
+            Nj = G[vs[j]].out
+
+            verts = [pi[v] for v in Nj - Ni if pi[v] < i]
+            if verts:
+                viable = [pi[v] for v in Ni - Nj]
+                if not viable or min(verts) <= min(viable):
+                    return False
+    return True
+
+
+def lexBFS(V, G):
+    order = [0 for _ in range(V)]
+    list_of_sets = [set([i for i in range(1, V + 1)])]
+    for i in range(1, V + 1):
+        current_vertex = list_of_sets[-1].pop()
+        order[i-1] = current_vertex
+        new_list_of_sets = []
+        for v_set in list_of_sets:
+            if v_set:
+                difference = v_set - G[current_vertex].out
+                multiplication = v_set & G[current_vertex].out
+                if difference:
+                    new_list_of_sets.append(difference)
+                if multiplication:
+                    new_list_of_sets.append(multiplication)
+    #     print('current vertext', current_vertex)
+    #     print('current vertext out', G[current_vertex].out)
+    #     print('current list', list_of_sets)
+    #     print('new list', new_list_of_sets)
+        list_of_sets = new_list_of_sets
+    # print(order)
+    # print(checkLexBFS(G, order))
+    return order
+
+
+def get_RN_list(order, V, G):
+    RN = []
+    for i in range(V):
+        RN.append([ order[j] for j in range (i) if order[j] in G[i+1].out])
+    return RN
+
+
+(V, L) = loadWeightedGraph('graphs/chordal/interval-rnd100')
+G = [None] + [Node(i) for i in range(1, V + 1)]
+for (u, v, _) in L:
+    G[u].connect_to(v)
+    G[v].connect_to(u)
+
+order = lexBFS(V, G)
+RN = get_RN_list(order, V, G)
